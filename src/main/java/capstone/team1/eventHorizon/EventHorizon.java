9dddb996b83b271140fc8b0724dd23b7ffecaa4a
StrwbryPanda <86024161+StrwbryPanda@users.noreply.github.com>
2025-02-17 18:14:44 -0500

    Created Template

---
+++ src/main/java/capstone/team1/eventHorizon/EventHorizon.java
@@ -0,0 +1,20 @@
+package capstone.team1.eventHorizon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventHorizon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+}
