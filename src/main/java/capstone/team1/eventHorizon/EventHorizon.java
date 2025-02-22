2445707c497fa047badb2a6ad30a5e99ebc9d97f
StrwbryPanda <86024161+StrwbryPanda@users.noreply.github.com>
2025-02-22 18:03:16 -0500

    Refactoring Week 1
    + Changed command structure from bukkit to PaperMC BasicCommands
    + Refactored commands, scoreboard, and timer
    + Created command package
    + Created config java class
    + Added reload and help commands

--- src/main/java/capstone/team1/eventHorizon/EventHorizon.java
+++ src/main/java/capstone/team1/eventHorizon/EventHorizon.java
@@ -1,15 +1,39 @@
 package capstone.team1.eventHorizon;
 
+<<<<<<< Updated upstream
+=======
+import capstone.team1.eventHorizon.commands.CommandsManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+>>>>>>> Stashed changes
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("UnstableApiUsage")
+
 public final class EventHorizon extends JavaPlugin {
+<<<<<<< Updated upstream
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         saveResource("config.yml", /* replace */ false);
+=======
+    public static EventHorizon plugin;
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+        // Plugin startup logic
+        capstone.team1.eventHorizon.ScoreboardManager scoreboardManager = new ScoreboardManager(this);
+        Bukkit.getPluginManager().registerEvents(new ScoreboardListener(this, scoreboardManager), this);
+
+        saveResource("config.yml", /* replace */ false);
+
+        //initializes eventhorizon base command
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS,
+                event -> event.registrar().register("eventhorizon", new CommandsManager(this)));
+>>>>>>> Stashed changes
     }
 
     @Override
---
+++ src/main/java/capstone/team1/eventHorizon/commands/CommandScoreboard.java
@@ -0,0 +1,6 @@
+package capstone.team1.eventHorizon.commands;
+
+//allows the user to toggle the scoreboard GUI
+public class CommandScoreboard
+{
+}
