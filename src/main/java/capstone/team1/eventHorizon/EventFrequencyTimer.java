eba9604d55c3e38e5a107727bfc0cdb779fea90f
alexfang452 <alexfang452@gmail.com>
2025-02-22 11:17:09 -0500

    Added java file + config changes.

---
+++ src/main/java/capstone/team1/eventHorizon/EventFrequencyTimer.java
@@ -0,0 +1,37 @@
+package capstone.team1.eventHorizon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import java.util.Random;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class EventFrequencyTimer {
+
+    private int eventFrequency;
+    private double posWeight;
+    private double negWeight;
+    private double neutralWeight;
+    private final Random random = new Random();
+
+    public EventFrequencyTimer(EventHorizon plugin, int eventFrequency, double posWeight, double negWeight, double neutralWeight){
+        FileConfiguration config = plugin.getConfig();
+
+        this.eventFrequency = config.getInt("event.frequency", 600);
+        this.posWeight = config.getDouble("event.posWeight", 0.2);
+        this.negWeight = config.getDouble("event.negWeight", 0.2);
+        this.neutralWeight = config.getDouble("event.neutralWeight", 0.6);
+    }
+
+    public void triggerEvent(){
+        double randomNumber = random.nextDouble();
+        if(randomNumber < posWeight){
+            Bukkit.broadcastMessage(ChatColor.GREEN + "Positive event");
+        }
+        else if(randomNumber < posWeight + negWeight){
+            Bukkit.broadcastMessage(ChatColor.RED + "Negative event");
+        }
+        else{
+            Bukkit.broadcastMessage(ChatColor.YELLOW + "Neutral event");
+        }
+    }
+}
