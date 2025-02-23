442b394a31afabfc3ad1082cfa546ee33204374b
StrwbryPanda <86024161+StrwbryPanda@users.noreply.github.com>
2025-02-22 19:20:50 -0500

    Fixed Merge Issues
    + Fixed merge issues
    + Added .gitignore

--- src/main/java/capstone/team1/eventHorizon/EventHorizon.java
+++ src/main/java/capstone/team1/eventHorizon/EventHorizon.java
@@ -1,52 +1,19 @@
 package capstone.team1.eventHorizon;
 
-<<<<<<< HEAD
 import capstone.team1.eventHorizon.commands.CommandsManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-=======
-<<<<<<< Updated upstream
-=======
 import capstone.team1.eventHorizon.commands.CommandsManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
->>>>>>> parent of a70912b (Resetting main)
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("UnstableApiUsage")
-
-<<<<<<< HEAD
 public final class EventHorizon extends JavaPlugin {
 
     public static EventHorizon plugin;
-
     @Override
     public void onEnable() {
-=======
-@SuppressWarnings("UnstableApiUsage")
-
-public final class EventHorizon extends JavaPlugin {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-        saveResource("config.yml", /* replace */ false);
-=======
-    public static EventHorizon plugin;
-    @Override
-    public void onEnable() {
-=======
-    public static EventHorizon plugin;
-    @Override
-    public void onEnable() {
->>>>>>> Stashed changes
->>>>>>> parent of a70912b (Resetting main)
         plugin = this;
 
         // Plugin startup logic
@@ -58,13 +25,6 @@ public final class EventHorizon extends JavaPlugin {
         //initializes eventhorizon base command
         this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS,
                 event -> event.registrar().register("eventhorizon", new CommandsManager(this)));
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
->>>>>>> parent of a70912b (Resetting main)
     }
 
     @Override
