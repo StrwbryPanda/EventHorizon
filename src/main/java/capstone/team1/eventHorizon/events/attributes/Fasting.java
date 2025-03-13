e469ab281fda66253cd6df5ee32f8480ddb10876
agouch <148926371+agouch@users.noreply.github.com>
2025-03-12 22:36:00 -0400

    Create Fasting.java

---
+++ src/main/java/capstone/team1/eventHorizon/events/attributes/Fasting.java
@@ -0,0 +1,20 @@
+package capstone.team1.eventHorizon.events.attributes;
+
+import capstone.team1.eventHorizon.events.EventClassification;
+import org.bukkit.entity.Player;
+
+public class Fasting extends AttributesBase {
+    public Fasting() {
+        super(EventClassification.NEGATIVE, "fasting");
+    }
+    @Override
+    public void applyEffect(Player player) {
+        player.setFoodLevel(0);
+        player.sendMessage("Your hunger has been completely depleted!");
+    }
+    @Override
+    public void execute() {
+        super.execute();
+    }
+
+}
