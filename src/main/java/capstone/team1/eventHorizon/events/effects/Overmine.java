d8fd196dd0420a7c2dfdcf661292cf461564ebf6
destinyj621 <draburnel621@gmail.com>
2025-03-16 13:44:08 -0400

    Updated effects events

--- src/main/java/capstone/team1/eventHorizon/events/effects/Overmine.java
+++ src/main/java/capstone/team1/eventHorizon/events/effects/Overmine.java
@@ -1,35 +1,25 @@
 package capstone.team1.eventHorizon.events.effects;
 
 import capstone.team1.eventHorizon.events.EventClassification;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-//Players can now mine twice as fast
-public class Overmine extends BaseEffects{
+/**
+ * Event that gives the player a haste effect for 6000 ticks (5 minutes)
+ */
+public class Overmine extends BaseEffects {
     public Overmine() {
         super(EventClassification.POSITIVE, "overmine");
+        addEffect(PotionEffectType.HASTE, 6000, 1,
+                false, true, true);
     }
 
     @Override
-    public void start() {
-        // Loop through all players and give potion
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.HASTE, globalDuration, globalAmplifier));
-        }
-
-        // Schedule the end method after globalDuration ticks
-        plugin.getServer().getScheduler().runTaskLater(plugin, this::end, globalDuration);
+    public void execute() {
+        super.execute();
     }
 
     @Override
-    public void end() {
-        // Loop through all players again and remove potion
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.removePotionEffect(PotionEffectType.HASTE);
-        }
+    public void terminate() {
+        super.terminate();
     }
-    public void terminate(){};
-
 }
--- src/main/java/capstone/team1/eventHorizon/events/effects/YoureTooSlow.java
+++ src/main/java/capstone/team1/eventHorizon/events/effects/YoureTooSlow.java
@@ -1,35 +1,25 @@
 package capstone.team1.eventHorizon.events.effects;
 
 import capstone.team1.eventHorizon.events.EventClassification;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-//Player receives slowness effect
-public class YoureTooSlow extends BaseEffects{
+/**
+ * Event that gives the player a slowness effect for 6000 ticks (5 minutes)
+ */
+public class YoureTooSlow extends BaseEffects {
     public YoureTooSlow() {
         super(EventClassification.NEGATIVE, "youreTooSlow");
+        addEffect(PotionEffectType.SLOWNESS, 6000, 1,
+                false, true, true);
     }
 
     @Override
-    public void start() {
-        // Loop through all players and give potion
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, globalDuration, globalAmplifier));
-        }
-
-        // Schedule the end method after globalDuration ticks
-        plugin.getServer().getScheduler().runTaskLater(plugin, this::end, globalDuration);
+    public void execute() {
+        super.execute();
     }
 
     @Override
-    public void end() {
-        // Loop through all players again and remove potion
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.removePotionEffect(PotionEffectType.SLOWNESS);
-        }
+    public void terminate() {
+        super.terminate();
     }
-    public void terminate(){};
-
 }
