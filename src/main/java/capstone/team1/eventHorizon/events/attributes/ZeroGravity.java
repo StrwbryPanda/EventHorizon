279949de36afccea718a36218630aef44f03260d
agouch <148926371+agouch@users.noreply.github.com>
2025-03-16 18:28:52 -0400

    Update ZeroGravity.java

--- src/main/java/capstone/team1/eventHorizon/events/attributes/ZeroGravity.java
+++ src/main/java/capstone/team1/eventHorizon/events/attributes/ZeroGravity.java
@@ -1,39 +1,32 @@
 package capstone.team1.eventHorizon.events.attributes;
 
 import capstone.team1.eventHorizon.events.EventClassification;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class ZeroGravity extends AttributesBase {
+public class ZeroGravity extends BaseAttribute {
 
     public ZeroGravity() {
         super(EventClassification.NEUTRAL, "zeroGravity");
-    }
 
-    @Override
-    public void applyEffect(Player player) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 200, 1));
-        player.sendMessage("You feel weightless as gravity fades away!");
-    }
 
-    /**
-     * Applies zero gravity to all entities nearby
-     */
-    public void applyZeroGravityToEntities() {
-        for (Entity entity : plugin.getServer().getWorlds().get(0).getEntities()) {
-            if (!(entity instanceof Player)) {
-                entity.setGravity(false);
-            }
-        }
+        addAttributeModifier(Attribute.GRAVITY, -0.05, AttributeModifier.Operation.ADD_NUMBER);
+        addAttributeModifier(Attribute.MOVEMENT_SPEED, 0.05, AttributeModifier.Operation.MULTIPLY_SCALAR_1);
     }
 
     @Override
     public void execute() {
         super.execute();
-        applyZeroGravityToEntities();
-    }
-    public void terminate(){};
 
+    }
+
+    @Override
+    public void terminate() {
+        super.terminate();
+    }
 }
+
