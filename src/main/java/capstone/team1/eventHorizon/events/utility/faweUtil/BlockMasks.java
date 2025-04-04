5085fb37b663e19dccb7e67e7331b76657f75c7d
StrwbryPanda <86024161+StrwbryPanda@users.noreply.github.com>
2025-04-04 11:11:53 -0400

    Minor adjustments

---
+++ src/main/java/capstone/team1/eventHorizon/events/utility/faweUtil/BlockMasks.java
@@ -0,0 +1,12 @@
+package capstone.team1.eventHorizon.events.utility.faweUtil;
+
+import com.sk89q.worldedit.function.mask.BlockMask;
+
+import java.util.List;
+
+public class BlockMasks
+{
+    public static List<BlockMask> groundBlocks;
+
+
+}
