c8e5d2fa5ea8b048c2279361ff60c2e0e8d630e1
StrwbryPanda <86024161+StrwbryPanda@users.noreply.github.com>
2025-04-10 11:18:20 -0400

    Added Jacoco and Junit

---
+++ src/test/java/capstone/team1/eventHorizon/SampleTest.java
@@ -0,0 +1,12 @@
+package capstone.team1.eventHorizon;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest
+{
+    @Test
+    public void testSample()
+    {
+        assertEquals(1, 1);
+    }
+}
